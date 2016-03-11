8bc8ef84431f699e1db5dcc39ca67ccc4159e987
Manu Mathew Thomas <manumathewthomas@github.com>
2016-03-10 20:15:30 -0600

    Billboards - trees,fog and clouds
    added billboards

---
+++ Coldre/Assets/Scripts/Billboard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+
+
+	public Vector3 Up;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.LookAt (Camera.main.transform.position, Vector3.up);
+	
+	}
+}
