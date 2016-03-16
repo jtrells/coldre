2b8db91ba4a938c4fb9d5ef0f0136f810e0f6a08
Carlos Uribe <curibe2@uic.edu>
2016-03-16 17:06:25 -0500

    Enemy Follow up
    The enemy (cube) will change color and start to follow when the player
    gets close to it

---
+++ Coldre/Assets/Scripts/ReturnToStart.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReturnToStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
