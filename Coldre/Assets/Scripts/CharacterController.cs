4c615a1956ede9c1a12eea738a388a170ea61d3d
Manu Mathew Thomas <manumathewthomas@github.com>
2016-03-15 21:28:59 -0500

    3rd person controller  and push action
    character controller, camera controller and push action

---
+++ Coldre/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour
+{
+
+	[System.Serializable]
+	public class MoveSettings
+	{
+		public float forwardVelocity = 12;
+		public float rotateVelocity = 100;
+		public float jumpVelocity = 25;
+		public float distToGrounded = 0.1f;
+		public LayerMask ground;
+	}
+
+	[System.Serializable]
+	public class PhysSettings
+	{
+		public float downAccelaration = 0.75f;
+	}
+
+	[System.Serializable]
+	public class InputSettings
+	{
+		public float inputDelay = 0.1f;
+	}
+
+
+	public MoveSettings moveSettings = new MoveSettings ();
+	public PhysSettings physSettings = new PhysSettings ();
+	public InputSettings inputSettings = new InputSettings ();
+
+
+	Vector3 velocity = Vector3.zero;
+	Quaternion targetRotation;
+	Rigidbody rBody;
+	float forwardInput, turnInput, jumpInput;
+
+// Use this for initialization
+	void Start ()
+	{
+		targetRotation = transform.rotation;
+
+		if (GetComponent<Rigidbody> ())
+			rBody = GetComponent<Rigidbody> ();
+		else
+			Debug.LogError ("No Rigidbody attachted");
+
+		forwardInput = turnInput = 0;
+	}
+
+// Update is called once per frame
+	void Update ()
+	{
+
+		GetInput ();
+		Turn ();
+	}
+
+	void FixedUpdate ()
+	{
+		Run ();
+		Jump ();
+
+		rBody.velocity = transform.TransformDirection (velocity);;
+	}
+
+	void GetInput ()
+	{
+		forwardInput = CAVE2Manager.GetAxis (1, CAVE2Manager.Axis.LeftAnalogStickUD);
+		turnInput = CAVE2Manager.GetAxis (1, CAVE2Manager.Axis.LeftAnalogStickLR);
+		//jumpInput = CAVE2Manager.GetButton (1, CAVE2Manager.Button.Button7) ? 1 : 0;
+		jumpInput = CAVE2Manager.GetButton (1, CAVE2Manager.Button.Button3) ? 1 : 0;
+
+
+		//jumpInput = Input.GetAxis("Jump_Trigger");
+
+	}
+
+	void Run ()
+	{
+		if (Mathf.Abs (forwardInput) > inputSettings.inputDelay) {
+			velocity.z = moveSettings.forwardVelocity * forwardInput;
+		} else
+			velocity.z = 0;
+	}
+
+	void Turn ()
+	{
+		if (Mathf.Abs (turnInput) > inputSettings.inputDelay) {
+			targetRotation *= Quaternion.AngleAxis (moveSettings.rotateVelocity * turnInput * Time.deltaTime, Vector3.up);
+		}
+		transform.rotation = targetRotation;
+
+
+	}
+
+	void Jump()
+	{
+		if (jumpInput > 0 && Grounded ()) {
+			velocity.y = moveSettings.jumpVelocity;
+		}
+		else if (jumpInput == 0 && Grounded ()) {
+			velocity.y=0;
+		}
+		else {
+			velocity.y -= physSettings.downAccelaration;
+		}
+	}
+
+	public Quaternion TargetRotation ()
+	{
+		return targetRotation;
+	}
+
+	bool Grounded()
+	{
+		return Physics.Raycast(transform.position,Vector3.down,moveSettings.distToGrounded,moveSettings.ground);
+	}
+}
---
+++ Coldre/Assets/Scripts/ColdreCameraController.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColdreCameraController : MonoBehaviour
+{
+	public Transform target;
+	public float lookSmooth = 0.09f;
+	public Vector3 offsetFromTarget = new Vector3 (0, 6, -8);
+
+	[System.Serializable]
+	public class PositionSettings
+	{
+		public Vector3 targetPosOffset = new Vector3 (0, 0, 0);
+		public float lookSmooth = 100f;
+		public float distanceFromTarget = -8f;
+		public float zoomSmooth = 100f;
+		public float maxZoom = -2f;
+		public float minZoom = -15f;
+
+	}
+
+	[System.Serializable]
+	public class OrbitSettings
+	{
+		public float xRotation = -20f;
+		public float yRotation = -180f;
+		public float maxXRotation = 25f;
+		public float minYRotation = -85f;
+		public float vOrbitSmooth = 150f;
+		public float hOrbitSmooth = 150f;
+	}
+
+	public PositionSettings position = new PositionSettings ();
+	public OrbitSettings orbit = new OrbitSettings ();
+
+	Vector3 targetPos = Vector3.zero;
+	Vector3 destination = Vector3.zero;
+	CharacterController characterController;
+	float vOrbitInput,hOrbitInput,zoomInput,hOrbitSnapInput;
+	float rotateVelocity = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		SetCameraTarget (target);
+
+		targetPos = target.position + position.targetPosOffset;
+		destination = Quaternion.Euler (orbit.xRotation, orbit.yRotation, 0) * -Vector3.forward * position.distanceFromTarget;
+		destination += targetPos;
+		transform.position = destination;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GetInput ();
+		OrbitTarget ();
+		ZoomInOnTarget ();
+	}
+
+	public void SetCameraTarget (Transform t)
+	{
+		target = t;
+
+		if (target != null) {
+			if (target.GetComponent<CharacterController> ()) {
+				characterController = target.GetComponent<CharacterController> ();
+			} else
+				Debug.LogError ("The Camera's target needs a character conttoller.");
+		} else
+			Debug.LogError ("Camera needs a target");
+	}
+
+	void GetInput()
+	{
+		vOrbitInput = CAVE2Manager.GetButton (4,CAVE2Manager.Button.SpecialButton2)?1:0;
+		if(vOrbitInput ==1)
+		Debug.Log (vOrbitInput);
+	}
+	void LateUpdate ()
+	{
+		MoveToTarget ();
+		LookAtTarget ();
+	}
+
+	void MoveToTarget ()
+	{
+//		targetPos = target.position + position.targetPosOffset;
+//		destination = Quaternion.Euler (orbit.xRotation, orbit.yRotation, 0) * -Vector3.forward * position.distanceFromTarget;
+//		destination += targetPos;
+//		transform.position = destination;
+
+		destination = characterController.TargetRotation () * offsetFromTarget;
+		destination += target.position;
+		transform.position = destination;
+	}
+
+	void LookAtTarget ()
+	{
+//		Quaternion targetRotation = Quaternion.LookRotation (targetPos - transform.position);
+//		transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, position.lookSmooth * Time.deltaTime);
+
+		float eulerYAngle = Mathf.SmoothDampAngle (transform.eulerAngles.y, target.eulerAngles.y, ref rotateVelocity, lookSmooth);
+		transform.rotation = Quaternion.Euler (transform.eulerAngles.x, eulerYAngle, 0);
+	}
+
+	void OrbitTarget()
+	{
+	}
+
+	void ZoomInOnTarget()
+	{
+
+	}
+}
