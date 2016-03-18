7ba985406e50a88fd337cf320972fe23c65f8479
Manu Mathew Thomas <manumathewthomas@github.com>
2016-03-18 03:15:55 -0500

    Demo Scene
    AI to Demo

---
+++ Coldre/Assets/Scripts/PlatformCollision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collision)
+	{
+
+		transform.GetComponent<FSMAI> ().state = FSMAI.State.Idle;
+	}
+
+	void OnTriggernExit(Collider collision)
+	{
+		transform.GetComponent<FSMAI> ().state = FSMAI.State.Action;
+	}
+}
