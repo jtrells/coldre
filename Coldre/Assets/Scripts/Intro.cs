b105c6d026df7d0cc3227b49629da6aa2e9b69a3
Manu Mathew Thomas <manumathewthomas@github.com>
2016-03-30 21:59:15 -0500

    integrated form of writing 1
    added intro scene with writer's input.

--- Coldre/Assets/Scripts/Intro.cs
+++ Coldre/Assets/Scripts/Intro.cs
@@ -35,15 +35,16 @@ public class Intro : MonoBehaviour {
 		Dialogs [0].GetComponent<Renderer> ().enabled = true;
 		Particles [0].GetComponent<Renderer> ().enabled = true;
 		Particles [0].GetComponent<ParticleSystem> ().startColor = new Color (255, 255, 255, 0.1f);
-		yield return new WaitForSeconds(2.0f);
+		yield return new WaitForSeconds(3.0f);
 		Particles [0].GetComponent<ParticleSystem> ().startColor = new Color (255, 255, 255, 0.15f);
 		Dialogs [1].GetComponent<Renderer> ().enabled = true;
-		yield return new WaitForSeconds(2.0f);
+		yield return new WaitForSeconds(4.0f);
 		Particles [1].GetComponent<Renderer> ().enabled = true;
 		Particles [0].GetComponent<ParticleSystem> ().startColor = new Color (255, 255, 255, 0.2f);
 		Particles [0].GetComponent<ParticleSystem> ().startColor = new Color (255, 255, 255, 0.2f);
 
 		Dialogs [0].GetComponent<Renderer> ().enabled = false;
+		Dialogs [1].GetComponent<Renderer> ().enabled = false;
 		Dialogs [2].GetComponent<Renderer> ().enabled = true;
 		yield return new WaitForSeconds(2.0f);
 
@@ -52,7 +53,65 @@ public class Intro : MonoBehaviour {
 
 		Dialogs [1].GetComponent<Renderer> ().enabled = false;
 		Dialogs [3].GetComponent<Renderer> ().enabled = true;
-		yield return new WaitForSeconds(5.0f);
+
+		yield return new WaitForSeconds(2.0f);
+		Dialogs [4].GetComponent<Renderer> ().enabled = true;
+		yield return new WaitForSeconds(2.0f);
+		Dialogs [5].GetComponent<Renderer> ().enabled = true;
+		yield return new WaitForSeconds(2.0f);
+		Dialogs [2].GetComponent<Renderer> ().enabled = false;
+		yield return new WaitForSeconds(1.0f);
+		Dialogs [3].GetComponent<Renderer> ().enabled = false;
+		yield return new WaitForSeconds(1.0f);
+		Dialogs [4].GetComponent<Renderer> ().enabled = false;
+		yield return new WaitForSeconds(1.0f);
+		Dialogs [5].GetComponent<Renderer> ().enabled = false;
+
+		yield return new WaitForSeconds(2.0f);
+		Dialogs [6].GetComponent<Renderer> ().enabled = true;
+		yield return new WaitForSeconds(2.0f);
+		Dialogs [7].GetComponent<Renderer> ().enabled = true;
+
+
+		yield return new WaitForSeconds(4.0f);
+		Dialogs [6].GetComponent<Renderer> ().enabled = false;
+		Dialogs [7].GetComponent<Renderer> ().enabled = false;
+
+
+		Dialogs [8].GetComponent<Renderer> ().enabled = true;
+		yield return new WaitForSeconds(2.0f);
+		Dialogs [9].GetComponent<Renderer> ().enabled = true;
+
+		yield return new WaitForSeconds(4.0f);
+		Dialogs [8].GetComponent<Renderer> ().enabled = false;
+		Dialogs [9].GetComponent<Renderer> ().enabled = false;
+
+		Dialogs [10].GetComponent<Renderer> ().enabled = true;
+		yield return new WaitForSeconds(2.0f);
+		Dialogs [11].GetComponent<Renderer> ().enabled = true;
+
+		yield return new WaitForSeconds(3.0f);
+		Dialogs [10].GetComponent<Renderer> ().enabled = false;
+		yield return new WaitForSeconds(1.0f);
+		Dialogs [11].GetComponent<Renderer> ().enabled = false;
+		yield return new WaitForSeconds(1.0f);
+		Dialogs [12].GetComponent<Renderer> ().enabled = true;
+		yield return new WaitForSeconds(2.0f);
+		Dialogs [13].GetComponent<Renderer> ().enabled = true;
+		yield return new WaitForSeconds(2.0f);
+		Dialogs [14].GetComponent<Renderer> ().enabled = true;
+		yield return new WaitForSeconds(2.0f);
+		Dialogs [15].GetComponent<Renderer> ().enabled = true;
+		yield return new WaitForSeconds(3.0f);
+		Dialogs [16].GetComponent<Renderer> ().enabled = true;
+
+
+
+
+
+
+
+
 		anim.SetBool("isIntro", true);
 		Dialogs [2].GetComponent<Renderer> ().enabled = false;
 		Dialogs [3].GetComponent<Renderer> ().enabled = false;
