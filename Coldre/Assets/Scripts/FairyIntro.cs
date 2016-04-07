eb79a5805e61219593ee4e78ef40874c9754ace8
Juan Trelles <juantrelles@gmail.com>
2016-04-07 10:21:38 -0500

    Music box, intro and fairy stories

--- Coldre/Assets/Scripts/FairyIntro.cs
+++ Coldre/Assets/Scripts/FairyIntro.cs
@@ -55,106 +55,137 @@ public class FairyIntro : MonoBehaviour {
 
 	public IEnumerator Dialog1()
 	{
-
-		
-
-		yield return new WaitForSeconds(2.0f);
+        // Fairy: Too fast!
+        yield return new WaitForSeconds(1.5f);
 		Dialogs [0].GetComponent<Renderer> ().enabled = true;
-		yield return new WaitForSeconds(2.0f);
+
+        // Fairy: I knew (gasp) that...
+        yield return new WaitForSeconds(1.5f);
 		Dialogs [0].GetComponent<Renderer> ().enabled = false;
 		Dialogs [1].GetComponent<Renderer> ().enabled = true;
-		yield return new WaitForSeconds(2.0f);
+
+        // Fairy: I (gasp) shouldn’t have (gasp) eaten
+        // all those (gasp) cookies for breakfast.
+        yield return new WaitForSeconds(5.2f);
 		Dialogs [1].GetComponent<Renderer> ().enabled = false;
 		Dialogs [2].GetComponent<Renderer> ().enabled = true;
-
 	}
 
 
 	public IEnumerator Dialog2()
 	{
-	
-		yield return new WaitForSeconds(5.0f);
+
+        // Fairy: Please (gasp) don’t (gasp) hurt me.
+        yield return new WaitForSeconds(3.5f);
 		Dialogs [2].GetComponent<Renderer> ().enabled = false;
 		Dialogs [3].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(2.0f);
+        // Rocky: Hurt you?
+        yield return new WaitForSeconds(2.0f);
 		Dialogs [3].GetComponent<Renderer> ().enabled = false;
 		Dialogs [4].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(2.0f);
+        // Rocky: We just wanted to know what’s going on
+        // around here and where’s Bella?
+        yield return new WaitForSeconds(4.5f);
 		Dialogs [4].GetComponent<Renderer> ().enabled = false;
 		Dialogs [5].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Cuddles: Stop it Rocky. Give her a second to catch her breath.
+        yield return new WaitForSeconds(3.2f);
 		Dialogs [5].GetComponent<Renderer> ().enabled = false;
 		Dialogs [6].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Cuddles:  She’s not looking fairy good right now. Don’t worry, we won’t hurt you.
+        yield return new WaitForSeconds(5.0f);
 		Dialogs [6].GetComponent<Renderer> ().enabled = false;
 		Dialogs [7].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Fairy: Thanks... 
+        // Wait a minute.
+        // How can you not know what’s going on?
+        // It's been total chaos in Coldre.
+        yield return new WaitForSeconds(7.4f);
 		Dialogs [7].GetComponent<Renderer> ().enabled = false;
 		Dialogs [8].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Cuddles: Well, the last thing I remember is playing with Bella here.
+        yield return new WaitForSeconds(3.8f);
 		Dialogs [8].GetComponent<Renderer> ().enabled = false;
 		Dialogs [9].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Cuddles:  It was a phenomenal game of hide-and-seek,
+        // I had just found the absolutely, positively, undeniably
+        // best hiding spot ever!
+        yield return new WaitForSeconds(7.5f);
 		Dialogs [9].GetComponent<Renderer> ().enabled = false;
 		Dialogs [10].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Cuddles: Unlike you, Rocky, 
+        // just hiding behind a flower while you take a nap.
+        yield return new WaitForSeconds(5.0f);
 		Dialogs [10].GetComponent<Renderer> ().enabled = false;
 		Dialogs [11].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Rocky: But I was tired.
+        yield return new WaitForSeconds(2.5f);
 		Dialogs [11].GetComponent<Renderer> ().enabled = false;
 		Dialogs [12].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Cuddles: Anyway, it was the best hiding place ever! 
+        // I am a great hider you know and—
+        yield return new WaitForSeconds(5.0f);
 		Dialogs [12].GetComponent<Renderer> ().enabled = false;
 		Dialogs [13].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Fairy: Get to the point!
+        yield return new WaitForSeconds(3.0f);
 		Dialogs [13].GetComponent<Renderer> ().enabled = false;
 		Dialogs [14].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Cuddles: Oh, yeah. 
+        // Well, I don’t remember what happened after that.
+        // Last I remember, the islands were still as beautiful and colorful as ever.
+        // Can you tell us what happened?
+        yield return new WaitForSeconds(9.5f);
 		Dialogs [14].GetComponent<Renderer> ().enabled = false;
 		Dialogs [15].GetComponent<Renderer> ().enabled = true;
 
-
-		yield return new WaitForSeconds(3.0f);
+        // Fairy: It was another beautiful day here on the islands, 
+        // when all of a sudden Bella disappeared and then the ogres came.
+        // They made the islands turn gloomy and dark. 
+        yield return new WaitForSeconds(10.5f);
 		Dialogs [15].GetComponent<Renderer> ().enabled = false;
 		Dialogs [16].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Cuddles: Maybe the ogres took Bella?
+        yield return new WaitForSeconds(3.5f);
 		Dialogs [16].GetComponent<Renderer> ().enabled = false;
 		Dialogs [17].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Rocky: Oh, you are making conclusions too fast. 
+        // It may be just a correlation, not a causation
+        // but lets add it to our theories list.
+        yield return new WaitForSeconds(10.0f);
 		Dialogs [17].GetComponent<Renderer> ().enabled = false;
 		Dialogs [18].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Fairy: I think you should explore the hills on the west side of the island.
+        // I hear that there is a mysterious place there.
+       // Maybe you’ll find Bella there or at least a way
+        yield return new WaitForSeconds(9.0f);
 		Dialogs [18].GetComponent<Renderer> ().enabled = false;
 		Dialogs [19].GetComponent<Renderer> ().enabled = true;
 
-		yield return new WaitForSeconds(3.0f);
+        // Cuddles: Well, we won’t figure anything out 
+        // by standing around.Let’s go!!
+        yield return new WaitForSeconds(4.5f);
 		Dialogs [19].GetComponent<Renderer> ().enabled = false;
 		Dialogs [20].GetComponent<Renderer> ().enabled = true;
 
-
-
-
-	
-
-
-
-
-	}
+        yield return new WaitForSeconds(4.5f);
+        Dialogs[20].GetComponent<Renderer>().enabled = false;
+    }
 
 
 
---
+++ Coldre/Assets/Scripts/StoryMusicBox.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StoryMusicBox : MonoBehaviour {
+
+    // public Transform[] Dialogs;
+    public Text[] Dialogs;
+	public Transform[] Particles;
+    public GameObject[] DialogPanels;
+	public Transform CAVECamera;
+	public Animator anim;
+    public AudioSource dialogue;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = CAVECamera.GetComponent<Animator>();
+
+		for(int i=0; i < Dialogs.Length; i++)
+			if(Dialogs[i]!=null) Dialogs[i].enabled = false;
+		
+		Particles [0].GetComponent<ParticleSystem> ().startColor = new Color (255, 255, 255, 0.0f);
+		Particles [0].GetComponent<Renderer> ().enabled = false;
+		Particles [1].GetComponent<ParticleSystem> ().startColor = new Color (255, 255, 255, 0.0f);
+		Particles [1].GetComponent<Renderer> ().enabled = false;
+		StartCoroutine (Dialog());
+	}
+
+
+	public IEnumerator Dialog()
+	{
+        //dialogue.Play();
+
+        yield return new WaitForSeconds(2.1f);
+
+        Particles[0].GetComponent<Renderer>().enabled = true;
+        Particles[0].GetComponent<ParticleSystem>().startColor = new Color(255, 255, 255, 0.1f);
+
+        Particles[0].GetComponent<ParticleSystem>().startColor = new Color(255, 255, 255, 0.15f);
+
+        Dialogs[0].enabled = true;
+        print(Dialogs[0].text);
+        yield return new WaitForSeconds(2.3f);
+        Dialogs[0].enabled = false;
+
+        Particles[1].GetComponent<Renderer>().enabled = true;
+        Particles[0].GetComponent<ParticleSystem>().startColor = new Color(255, 255, 255, 0.2f);
+        Particles[0].GetComponent<ParticleSystem>().startColor = new Color(255, 255, 255, 0.2f);
+
+        Dialogs[1].enabled = true;
+        print(Dialogs[1].text);
+        yield return new WaitForSeconds(2.3f);
+        Dialogs[1].enabled = false;
+
+        Particles[0].GetComponent<ParticleSystem>().startColor = new Color(255, 255, 255, 0.5f);
+        Particles[1].GetComponent<ParticleSystem>().startColor = new Color(255, 255, 255, 0.5f);
+
+        Dialogs[2].enabled = true;
+        yield return new WaitForSeconds(2.3f);
+        Dialogs[2].enabled = false;
+
+        Dialogs[3].enabled = true;
+        yield return new WaitForSeconds(2.3f);
+        Dialogs[3].enabled = false;
+
+        Dialogs[4].enabled = true;
+        yield return new WaitForSeconds(2.3f);
+        Dialogs[4].enabled = false;
+
+        Dialogs[5].enabled = true;
+        yield return new WaitForSeconds(2.3f);
+        Dialogs[5].enabled = false;
+
+        Dialogs[6].enabled = true;
+        yield return new WaitForSeconds(2.3f);
+        Dialogs[6].enabled = false;
+
+        Dialogs[7].enabled = true;
+        yield return new WaitForSeconds(2.3f);
+        Dialogs[7].enabled = false;
+
+        Dialogs[8].enabled = true;
+        yield return new WaitForSeconds(2.3f);
+        Dialogs[8].enabled = false;
+
+        Dialogs[9].enabled = true;
+        yield return new WaitForSeconds(2.3f);
+        Dialogs[9].enabled = false;
+
+        Dialogs[10].enabled = true;
+        yield return new WaitForSeconds(2.3f);
+        Dialogs[10].enabled = false;
+
+        //dialogue.Stop();
+
+        Particles[0].GetComponent<ParticleSystem>().startColor = new Color(255, 255, 255, 0.2f);
+        Particles[1].GetComponent<ParticleSystem>().startColor = new Color(255, 255, 255, 0.2f);
+        yield return new WaitForSeconds(2.0f);
+        Particles[0].GetComponent<ParticleSystem>().startColor = new Color(255, 255, 255, 0.01f);
+        Particles[1].GetComponent<ParticleSystem>().startColor = new Color(255, 255, 255, 0.01f);
+    }
+}
